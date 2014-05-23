b0d78a671cf9b4ded048d7d0ad4f65651a77e95c
KyorCode <t-c@live.be>
2014-05-23 23:28:46 +0200

    Readme to HTML

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var markdown = require('gulp-markdown');
+
+gulp.task('docs', function () {
+    gulp.src('README.md')
+        .pipe(markdown())
+    pipe(gulp.dest('docs'));
+});
+
+gulp.task('default',['docs']);
