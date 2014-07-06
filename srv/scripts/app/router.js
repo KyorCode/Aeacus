ce7ef0a80df3dac9a6728fb6ceb7d0e8acd21cf3
KyorCode <t-c@live.be>
2014-07-06 21:33:09 +0200

    Some ember routing

--- srv/scripts/app/router.js
+++ srv/scripts/app/router.js
@@ -1,7 +1,15 @@
 Aeacus.Router.map(function () {
-    this.resource('aeacus', {path: '/'},function(){
+    this.resource('aeacus', {path: '/'}, function () {
         this.route('dashboard');
+
+
         this.route('bankaccounts');
+        this.route('bankaccount.new', {path: '/bankaccount/new'});
+        this.resource('aeacus.bankaccount', {path: '/bankaccount/:bankaccount_id'}, function () {
+            this.route('edit');
+        });
+
+
         this.route('invoices');
         this.route('creditnotes');
         this.route('customers');
