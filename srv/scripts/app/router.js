a85214e18495ffec712285e54d77acd128d6bea3
KyorCode <t-c@live.be>
2014-07-17 00:08:22 +0200

    Finally some layout progress

---
+++ srv/scripts/app/router.js
@@ -0,0 +1,13 @@
+Aeacus.Router.map(function(){
+    this.resource('aeacus',{path : '/'},function(){
+
+        this.route('dashboard', {path:'/'});
+
+        this.route('calendar');
+
+        this.route('invoices');
+        this.route('creditnotes');
+        this.route('customers');
+
+    });
+});
